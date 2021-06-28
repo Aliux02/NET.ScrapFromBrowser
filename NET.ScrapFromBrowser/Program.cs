7a5ce2a8eaad66d9e6eb281fb8319d0ffb0d7cd6
Aliux <aliux02@gmail.com>
2021-06-28 20:06:14 +0300

    Add project files.

---
+++ NET.ScrapFromBrowser/Program.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace NET.ScrapFromBrowser
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://www.cvonline.lt/lt/search?limit=20&offset=0&categories%5B0%5D=INFORMATION_TECHNOLOGY&towns%5B0%5D=540&isHourlySalary=false&isRemoteWork=false";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var nodes = doc.DocumentNode.SelectNodes("//span[contains(@class, 'jsx-1471379408 vacancy-item__title')]");
+
+            var professionNames = nodes.Where(node => node.InnerText.Contains("CONSULTANT"));
+
+            foreach (HtmlNode item in professionNames)
+            {
+                Console.WriteLine(item.InnerText);
+            }
+        }
+    }
+}
